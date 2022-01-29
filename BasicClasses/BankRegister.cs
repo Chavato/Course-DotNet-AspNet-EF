305252d892d84d9eb7743836bedb79f85502ac9a
Rafael Santos <rafaelchaves@unifei.edu.br>
2022-01-29 01:03:04 -0300

    Create BankRegister.cs

---
+++ BasicClasses/BankRegister.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace BasicClasses
+{
+    class BankRegister
+    {
+
+        public double MoneyBalance { get; private set; }
+        public int AccNumber { get; private set; }
+        public string Name { get; set; }
+
+        public BankRegister(int accNumber, string name)
+        {
+            AccNumber = accNumber;
+            Name = name;
+
+        }
+        public BankRegister(int accNumber, string name, double initialMoney) : this(accNumber, name)
+        {
+            PostMoney(initialMoney);
+
+        }
+
+        public void PostMoney(double money)
+        {
+            MoneyBalance += money;
+            ShowInfoAcc();
+        }
+
+        public void GetMoney(double money)
+        {
+            MoneyBalance -= money + 5;
+            ShowInfoAcc();
+        }
+
+        public void ShowInfoAcc()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Account Information");
+            System.Console.WriteLine("Account Number: {0}, Name: {1}, Balance: {2}", AccNumber, Name, MoneyBalance);
+            System.Console.WriteLine();
+        }
+    }
+
+}
