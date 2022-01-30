e5e9f0e3fdd746b27004eaeb30fdf2007372f65a
Rafael Santos <rafaelchaves@unifei.edu.br>
2022-01-30 12:13:09 -0300

    Class 122 exercise

---
+++ Entities/Class-122/Comment.cs
@@ -0,0 +1,13 @@
+namespace Entities.Class122
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+
+}
